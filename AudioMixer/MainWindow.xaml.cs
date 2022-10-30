43048d5c23170df14cc5a2fd16fa28e7bb2ca1ba
drwoj <dr.woj@wp.pl>
2022-10-30 22:15:26 +0100

    Created basic GUI and wav. file loading

---
+++ AudioMixer/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace AudioMixer
+{
+    public partial class MainWindow : Window
+    {
+
+        [DllImport(@"C:\Users\Piotr\source\repos\AudioMixer\x64\Debug\Mixing.dll")]
+        static extern int MyProc1(int a, int b);
+            Mixer mixer = new Mixer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Wav Files|*.wav";            
+            Button btn = sender as Button;
+
+            if (ofd.ShowDialog() == true)
+            {
+                btn.Content = System.IO.Path.GetFileNameWithoutExtension(ofd.FileName);
+                mixer.setReader(btn.Name, ofd.FileName);
+            }
+        }
+
+        private void btnMixFiles_Click(object sender, RoutedEventArgs e)
+        {
+            mixer.initBuffers();
+        }
+    }
+}
---
+++ AudioMixer/Mixer.cs
@@ -0,0 +1,38 @@
+﻿using NAudio.Wave;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioMixer
+{
+    class Mixer
+    {
+        WaveFileReader wavReader, wavReader2;
+        short[] sampleBuffer, sampleBuffer2;
+
+        public void setReader(string index, string path) {
+            if(index == "b1")
+                wavReader = new WaveFileReader(path);
+            if (index == "b2")
+                wavReader2 = new WaveFileReader(path);
+        }
+
+
+        public void initBuffers()
+        {
+            initBuffer(wavReader, sampleBuffer);
+            initBuffer(wavReader2, sampleBuffer2);
+        }
+
+        private void initBuffer(WaveFileReader reader, short[] buffer)
+        {
+            byte[] tempBuffer = new byte[reader.Length];
+            int read = reader.Read(tempBuffer, 0, buffer.Length);
+            buffer = new short[read / 2];
+            Buffer.BlockCopy(buffer, 0, buffer, 0, read);
+        }
+    }
+}
